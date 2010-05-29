5e1c7508ea5c8bea6fb3cd3d8e029259b9ebe228
Rich Lovely <rich@roadierich.co.uk>
2010-05-29 14:27:59 +0100

    Adding some Vote classes

--- game.py
+++ game.py
@@ -19,46 +19,133 @@ from roles import *
 
 from random import choice, shuffle
 from itertools import imap, izip
+from collections import defaultdict
 
 def C9RoleFact():
-	baseRoles = [Townie, Townie, Townie, Mafia, Mafia]
-	randomRoles = [[Townie, Townie],
-		   [Townie, Cop],
-		   [Doctor, Townie],
-		   [Doctor, Cop]]
-	return iter(baseRoles + choice(randomRoles))
+    baseRoles = [Townie, Townie, Townie, Mafia, Mafia]
+    randomRoles = [[Townie, Townie],
+           [Townie, Cop],
+           [Doctor, Townie],
+           [Doctor, Cop]]
+    return iter(baseRoles + choice(randomRoles))
 
 class Game:
-	"""vanilla c9 game by default. When we add more games later we might
-	want to change this into an abstract base class and/or add the option
-	for passing a setup to the Game class"""
+    """vanilla c9 game by default. When we add more games later we might
+    want to change this into an abstract base class and/or add the option
+    for passing a setup to the Game class"""
 
-	def __init__(self, players, roles=C9RoleFact):
-		"""Constructor. Starts the game at D1 and is initialized with
-		the player list"""
-		shuffle(players)
-		
-		try:
-			rolelist = roles()
-		except TypeError:
-			rolelist = roles
-		
-		self.roles = dict((player, role(player)) for player, role in izip(players, rolelist))
-		
-	def action(self, player, action, args):
-		"""Whenever a player does an action, it's passed to his role.
-		This includes votes."""
-		try:
-			act = getattr(self.roles[player],"action_%s" % action)  #TODO: Convert give the Role class some means to convert the string aruments into players/Roles
-		except KeyError: #The person who did the action is not a player. Ignore him.
-			pass
-		if act is None: #There exists no such action for that player. Inform the mod if there is one, and continue
-			raise Manual_Action("%s %ss %s" % (player, action, ' '.join(args)))
-		act(args)
+    def __init__(self, thingy, players, roles=C9RoleFact):
+        """Constructor. Starts the game at D1 and is initialized with
+        the player list"""
+        
+        self.thingy = thingy
+        self.vote = LynchVote(self)
+        
+        shuffle(players)
+        
+        try:
+            rolelist = roles()
+        except TypeError:
+            rolelist = roles
+        
+        self.roles = dict((player, role(self, player)) for player, role in izip(players, rolelist))
+        
+    def action(self, player, action, args):
+        """Whenever a player does an action, it's passed to his role.
+        This includes votes."""
+        try:
+            act = getattr(self.roles[player],"action_%s" % action)  #TODO: Convert give the Role class some means to convert the string aruments into players/Roles
+        except KeyError: #The person who did the action is not a player. Ignore him.
+            pass
+        if act is None: #There exists no such action for that player. Inform the mod if there is one, and continue
+            raise Manual_Action("%s %ss %s" % (player, action, ' '.join(args)))
+        act(args)
+
+class VoteContainer:
+    """Simple container holding a count and a list of people who voted that way"""
+    def __init__(self):
+        self.count = 0
+        self.votes = []
+    def append(voter):
+        """add a vote from voter, with a voting power of count"""
+        self.votes.append(voter)
+        self.count += voter.votePower
+    def remove(voter):
+        """remove the vote from voter, with a voting power of count"""
+        try:
+            self.votes.remove(voter)
+            self.count -= voter.votePower
+        except ValueError:
+            pass
+        
+    def __str__(self):
+        if self.count == 0:
+            return "0"
+        else:
+            pnameGet = lambda p: p.pname
+            return "%s (%s)" % (self.count, ", ".join(map(pnameGet, self.votes)))
+
+class VoteBase(object):
+    def __init__(self, game):
+        self.game = game
+        self.votes = defaultdict(VoteContainer)
+        self.options = set()
+        
+    def voteFor(self,voter,candidate):
+        """register a vote by voter for candidate"""
+        if not candidate in self.options:
+            raise ThingyError("Invalid vote")
+        self.votes[candidate].append(voter)
+        game.say(self.votals())
+        self.checkMajority()
+            
+
+    def unvote(voter,candidate):
+        if not candidate in self.options:
+            raise ThingyError("Invalid unvote")
+        self.votes[candidate].remove(voter)
+        
+    def checkMajority(self):
+        pass
+        
+    def votals(self, for_=None):
+        if for_ is not None:
+            if for_ in self.votes and self.votes[for_].count < 0:
+                return "%s: %s" % (for_.pname, self.votes[for_])
+            else:
+                return "%s has no votes" % for_.pname
+        else:
+            op = []
+            for player, votes in self.votes.iteritems():
+                if votes.count:
+                    op.append("%s: %s" % (player.pname, votes))
+            return " ".join(op)
+
+class LynchVote(VoteBase):
+    def __init__(self, game):
+        VoteBase.__init__(self, game)
+        self.options = set(game.playerList)
+        self.options.add("NL")
+        self.majority = 1 + len(self.options)/2 #int division ftw
+    
+    def checkMajority(self):
+        for player, votes in self.votes.iteritems():
+            if votes.count >= self.majority:
+                raise Manual_Action("lynch vote majority reached for %s" % player)
+
+class MafiaKillVote(VoteBase):
+    def __init__(self, game):
+        VoteBase.__init__(self, game)
+        self.options = set(game.playerList)
+        self.options.add("NL")
+        self.majority = 1 + game.mafiaCount/2 #int division ftw
+
+    def voteFor(self, voter, candidate):
+        if 
 
 if __name__ == "__main__":
-	print "Running test of Game.py"
-	Players = ["P1", "P2", "P3", "P4", "P5", "P6", "P7"]
-	c9 = Game(Players)
-	print c9.roles
-	c9.action("P1", "vote", ["P2"])
+    print "Running test of Game.py"
+    Players = ["P1", "P2", "P3", "P4", "P5", "P6", "P7"]
+    c9 = Game(Players)
+    print c9.roles
+    c9.action("P1", "vote", ["P2"])
--- roles.py
+++ roles.py
@@ -10,113 +10,124 @@ from base import *
 from game import *
 
 class Manual_Action(Exception):
-	"""Thrown when there's no automated method for handling a night action. The Game class catches and asks the moderator what to do"""
+    """Thrown when there's no automated method for handling a night action. The Game class catches and asks the moderator what to do"""
 
 class Role:
-	"""Base role class. Only counts votes to assist a human moderator"""
-	def __init__(self, pname):
-		self.name = "Role"
-		self.pname = pname
-		self.alignment = None
-	
-	#TODO: Vote counting
-	
+    """Base role class. Only counts votes to assist a human moderator"""
+    def __init__(self, game, pname):
+        self.game = game
+        self.name = "Role"
+        self.pname = pname
+        self.alignment = None
+
+    #the following methods are required to use Roles as dict keys       
+    def __hash__(self):
+        return hash(self.pname)
+    def __eq__(self,other):
+        return self.pname == other.pname
+    
+    #TODO: Vote counting
+    
 class Vanilla_Role(Role):
-	"""General Vanilla_Role class, for vannila games"""
-	def __init__(self, pname):
-		Role.__init__(self, pname)
-		self.alive = True
-		self.name = "Vanilla Role"
-		self.alignment = "town"
-		self.reset()
+    """General Vanilla_Role class, for vannila games"""
+    def __init__(self, game, pname):
+        Role.__init__(self, game, pname)
+        self.alive = True
+        self.name = "Vanilla Role"
+        self.alignment = "town"
+        self.votedFor = None
 
-	def process(self):
-		if self.killed:
-			return self.doctored
-		else:
-			return True
-	
-	def reaction_mafia_kill(self, mafia):
-		self.killed = True
+        self.reset()
 
-	def reaction_doctor(self, doctor):
-		self.doctored = True
+    def process(self):
+        if self.killed:
+            return self.doctored
+        else:
+            return True
+    
+    def reaction_mafia_kill(self, mafia):
+        self.killed = True
 
-	def reset(self):
-		self.killed = False
-		self.doctored = False
-		
-	def action_vote(self, Target):
-		pass
+    def reaction_doctor(self, doctor):
+        self.doctored = True
+
+    def reset(self):
+        self.killed = False
+        self.doctored = False
+        
+    def action_vote(self, target):
+        game.vote.unvote(self.pname, self.votedFor)
+        game.vote.voteFor(self.pname, target)
+        self.votedFor = target
 
 class Mafia(Vanilla_Role):
-	"""Standard mafia goon"""
-	def __init__(self, pname):
-		Vanilla_Role.__init__(self, pname)
-		self.name = "Mafia"
-		self.alignment = "scum"
-	
-	def action_mafia_kill(self, target):
-		try:
-			result = target.reaction_mafia_kill(self)
-		except AttributeError:
-			raise Manual_Action("%s %ss %s" % (self.pname, "mafia_kill", target.pname))
-		target.killed = True
+    """Standard mafia goon"""
+    def __init__(self, game, pname):
+        Vanilla_Role.__init__(self, game, pname)
+        self.name = "Mafia"
+        self.alignment = "scum"
+    
+    def action_mafia_kill(self, target):
+        try:
+            result = target.reaction_mafia_kill(self)
+        except AttributeError:
+            raise Manual_Action("%s %ss %s" % (self.pname, "mafia_kill", target.pname))
+        target.killed = True
 
 class Townie(Vanilla_Role):
-	"""Vanilla townie"""
-	def __init__(self, pname):
-		Vanilla_Role.__init__(self, pname)
-		self.name = "Townie"
+    """Vanilla townie"""
+    def __init__(self, game, pname):
+        Vanilla_Role.__init__(self, game, pname)
+        self.name = "Townie"
 
 class Cop(Vanilla_Role):
-	"""Sane cop"""
-	def __init__(self, pname):
-		Vanilla_Role.__init__(self, pname)
-		self.name = "Cop"
-		
-	def action_cop(self, target):
-		try: target.reaction_cop(self)
-		except AttributeError: pass
-		return target.alignment
+    """Sane cop"""
+    def __init__(self, game, pname):
+        Vanilla_Role.__init__(self, game, pname)
+        self.name = "Cop"
+        
+    def action_cop(self, target):
+        try: target.reaction_cop(self)
+        except AttributeError: pass
+        return target.alignment
 
 class Doctor(Vanilla_Role):
-	"""Sane cop"""
-	def __init__(self, pname):
-		Vanilla_Role.__init__(self, pname)
-		self.name = "Cop"
-		
-	def action_doctor(self, target):
-		try:
-			target.reaction_doctor(self)
-		except AttributeError:
-			raise Manual_Action("%s %ss %s" % (self.pname, "doctor", target.pname))
+    """Sane cop"""
+    def __init__(self, game, pname):
+        Vanilla_Role.__init__(self, game, pname)
+        self.name = "Cop"
+        
+    def action_doctor(self, target):
+        try:
+            target.reaction_doctor(self)
+        except AttributeError:
+            raise Manual_Action("%s %ss %s" % (self.pname, "doctor", target.pname))
 
 class Paranoid_gunowner(Vanilla_Role):
-	"""Paranoid gun owner. Kills any cop that targets him"""
-	#this class is intended as a demo for reactions
-	def __init__(self, pname):
-		Vanilla_Role.__init__(self, pname)
-		self.name = "Paranoid Gun Owner"
-		
-	def reaction_cop(self, targetingCop):
-		targetingCop.killed = True
+    """Paranoid gun owner. Kills any cop that targets him"""
+    #this class is intended as a demo for reactions
+    def __init__(self, pname):
+        Vanilla_Role.__init__(self, game, pname)
+        self.name = "Paranoid Gun Owner"
+        
+    def reaction_cop(self, targetingCop):
+        targetingCop.killed = True
 
 if __name__ == "__main__":
-	m = Mafia("P1")
-	t = Townie("P2")
-	c = Cop("P3")
-	x = Role("P4")
-	try:
-		c.action_cop(m)
-		c.action_cop(t)
-		c.action_cop(x)
-	except Manual_Action as MA:
-		print MA
-		
-	try:
-		m.action_mafia_kill(c)
-		m.action_mafia_kill(t)
-		m.action_mafia_kill(x)
-	except Manual_Action as MA:
-		print MA
+    m = Mafia("P1")
+    t = Townie("P2")
+    c = Cop("P3")
+    x = Role("P4")
+    try:
+        c.action_cop(m)
+        c.action_cop(t)
+        c.action_cop(x)
+    except Manual_Action as MA:
+        print MA
+        
+    try:
+        m.action_mafia_kill(c)
+        m.action_mafia_kill(t)
+        m.action_mafia_kill(x)
+    except Manual_Action as MA:
+        print MA
